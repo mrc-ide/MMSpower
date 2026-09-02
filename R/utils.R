626688a10779dd4ba52e2f2e6e345956848f8834
Sequoia Leuba <sequoia.leuba@gmail.com>
2026-09-02 14:58:10 +0100

    design-detection: drop dead .sampling_design from utils.R (housekeeping)

--- R/utils.R
+++ R/utils.R
@@ -2,7 +2,7 @@
 # None of these are exported.
 
 # Wilson score confidence interval for a proportion.
-# Returns list(lower, upper).
+# Returns list(lower, upper). Used by test_difference() (Newcombe interval).
 .wilson_ci <- function(p, n, z) {
   denom  <- 1 + z^2 / n
   center <- (p + z^2 / (2 * n)) / denom
@@ -18,35 +18,4 @@
 # Apparent prevalence -> true prevalence (inverse Rogan-Gladen).
 .rogan_gladen <- function(p_apparent, sensitivity, specificity) {
   (p_apparent - (1 - specificity)) / (sensitivity + specificity - 1)
-}
-
-# Compute design effect (DEFF) from ICC and cluster size.
-#
-# Returns a list:
-#   deff       -- Kish design effect (>= 1); 1 means no clustering
-#   n_per_site -- cluster size used (may be NULL)
-#   n_sites    -- number of sites supplied (may be NULL)
-#
-# Planned but not yet implemented:
-#   allocation = "pps"  -- proportional-to-size allocation
-#   weighting  = "site" -- site-level weighting
-.sampling_design <- function(
-  n_per_site = NULL,
-  n_sites    = NULL,
-  icc        = 0,
-  allocation = c("equal", "pps"),
-  weighting  = c("none", "site")
-) {
-  allocation <- match.arg(allocation)
-  weighting  <- match.arg(weighting)
-
-  if (allocation == "pps")  stop("PPS allocation is not yet implemented")
-  if (weighting  == "site") stop("Site weighting is not yet implemented")
-
-  deff <- 1
-  if (!is.null(n_per_site) && n_per_site > 1 && icc > 0) {
-    deff <- 1 + (n_per_site - 1) * icc   # Kish (1965) formula
-  }
-
-  list(deff = deff, n_per_site = n_per_site, n_sites = n_sites)
 }
